cdaad3996581a2057bf15937b0b04d28b541423f
Pfarelo Nelwamondo <Pfare5057@gmail.com>
2024-02-25 19:07:37 +0200

    finished date count

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import "./index.css";
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  function handleMinusStep() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+  function handlePlusStep() {
+    setStep((s) => s + 1);
+  }
+
+  function handleMinusCount() {
+    setCount((c) => c - step);
+  }
+  function handlePlusCount() {
+    setCount((c) => c + step);
+  }
+
+  return (
+    <div className="container">
+      <div>
+        <button onClick={handleMinusStep}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={handlePlusStep}>+</button>
+      </div>
+      <div>
+        <button onClick={handleMinusCount}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={handlePlusCount}>+</button>
+      </div>
+
+      <p>
+        <DateGen day={count} />
+      </p>
+    </div>
+  );
+}
+
+function DateGen(props) {
+  const currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() + props.day);
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const formattedDate = `${days[currentDate.getDay()]} ${
+    months[currentDate.getMonth()]
+  } ${currentDate.getDate()} ${currentDate.getFullYear()}`;
+
+  return (
+    <div>
+      <span>{props.day} </span>
+      <span>days from today is </span>
+      <span>{formattedDate} </span>
+    </div>
+  );
+}
